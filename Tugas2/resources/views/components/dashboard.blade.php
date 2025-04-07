6d6ed28ff6406e5a3fd83f0bf7ee18ef90d4914b
yohanpratamaa <yohanpratamaa@gmail.com>
2025-04-07 13:26:48 +0700

    add sidebar and dashboard outline

---
+++ Tugas2/resources/views/components/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-between w-full h-full">
+        <div class="w-[300px] h-[150px] border bg-orange-500">
+            <p>Hello</p>
+        </div>
+    </div>
+@endsection
